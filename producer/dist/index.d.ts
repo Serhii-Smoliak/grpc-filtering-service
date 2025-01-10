9754bf45b7e7f43f0f7ac979af221ba2df99c8d1
Smoliak Serhii <full.stack.spcreator@gmail.com>
2025-01-10 06:15:57 +0200

    changed-gitignore-v2

--- producer/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './producer.service';
-export * from './producer.module';
--- producer/dist/producer.controller.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class ProducerController {
-    getFilteredUsers(): Promise<{
-        users: any;
-    }>;
-}
